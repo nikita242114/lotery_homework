c3c663a91e5eac5fe5932b793203ca3c3cba2df8
nikita242114 <5358711@mail.ru>
2023-11-10 12:47:08 +0500

    work1

--- ToyRaffle.java
+++ ToyRaffle.java
@@ -2,11 +2,11 @@ public class ToyRaffle {
     public static void main(String[] args) {
         ToyStore store = new ToyStore();
 
-        Toy toy1 = new Toy(1, "Конструктор Lego", 10, 20);
-        Toy toy2 = new Toy(2, "Трансфомер", 200, 40);
-        Toy toy3 = new Toy(3, "Мяч", 200, 90);
-        Toy toy4 = new Toy(4, "Синий трактор ", 50, 30);
-        Toy toy5 = new Toy(5, "Кукла черепашки ниндзя ", 200, 90);
+        Toy toy1 = new Toy(1, "Конструктор Lego", 10, 30);
+        Toy toy2 = new Toy(2, "Трансфомер", 100, 50);
+        Toy toy3 = new Toy(3, "Мяч", 150, 90);
+        Toy toy4 = new Toy(4, "Синий трактор ", 60, 30);
+        Toy toy5 = new Toy(5, "Кукла черепашки ниндзя ", 160, 90);
 
         store.addToy(toy1);
         store.addToy(toy2);
